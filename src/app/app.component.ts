27fa718a099f3f0a7cad03276e5ee0a0447bbbac
LukasReil <lukas_reil0305@gmx.net>
2021-06-28 14:20:22 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { WeatherService } from './weather.service';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor() {}
-
-  
-  
+  constructor(weatherservice:WeatherService) {}
 }
